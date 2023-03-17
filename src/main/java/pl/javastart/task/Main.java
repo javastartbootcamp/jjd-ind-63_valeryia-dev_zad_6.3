15a18a6e0f4ac9acc38aec30898213a6710c4905
Valeryia Paliashchuk <vallery7543@gmail.com>
2023-03-17 19:41:35 +0100

    Rozwiązanie

--- src/main/java/pl/javastart/task/Main.java
+++ src/main/java/pl/javastart/task/Main.java
@@ -3,6 +3,9 @@ package pl.javastart.task;
 public class Main {
 
     public static void main(String[] args) {
+        ArrayUtils utils = new ArrayUtils();
+        int [] [] tab = utils.createArray();
+        utils.printInfo(tab);
 
     }
 }
